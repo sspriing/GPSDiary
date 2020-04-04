8e33e8d6b5dc2abf3111dae25f8caab76f2f0c70
SeoBomi <bomi2006@naver.com>
2020-04-04 12:35:01 +0900

    firestore연결

--- app/src/main/java/com/example/gpsdiary/MainActivity.kt
+++ app/src/main/java/com/example/gpsdiary/MainActivity.kt
@@ -3,20 +3,20 @@ package com.example.gpsdiary
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gpsdiary.date.DateAdapter
 import com.example.gpsdiary.date.DateClass
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 
 
 class MainActivity : AppCompatActivity() {
 
-    val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    val myRef : DatabaseReference = database.getReference("mesage")
+    val database : FirebaseFirestore = FirebaseFirestore.getInstance()
+    val TAG = "MyMessage"
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,8 +35,24 @@ class MainActivity : AppCompatActivity() {
         recycle_date.layoutManager = LinearLayoutManager(this)
 
         button.setOnClickListener {
-            myRef.child("User").push().setValue("Me")
-            myRef.child("User").child("date").push().setValue("Today")
+            database.collection("User")
+                .add("me")
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot add with ID: ${documentReference}")
+                }
+                .addOnFailureListener{e->
+                    Log.w(TAG, "Error adding document", e)}
+            database.collection("User")
+                .get()
+                .addOnSuccessListener { result->
+                    for(document in result){
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener{exception ->
+                    Log.w(TAG, "Error getting documents", exception)
+                }
+
         }
 
     }
